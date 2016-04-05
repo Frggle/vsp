48e11f7c997cb228e381725b9d78d97085a09b35
Jenny <jenny2209@gmx.de>
2016-04-05 10:21:48 +0200

    Dependency Spark hinzugefügt

--- VSPraktikum/src/main/java/haw/vs/VSPraktikum/App.java
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/App.java
@@ -1,13 +1,8 @@
 package haw.vs.VSPraktikum;
+import static spark.Spark.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
     }
 }
