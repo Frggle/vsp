11e0e27a854b729946499b0303b2a1be09bd676d
Jenny <jenny2209@gmx.de>
2016-04-05 10:07:21 +0200

    Projekt angelegt

---
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/App.java
@@ -0,0 +1,13 @@
+package haw.vs.VSPraktikum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
