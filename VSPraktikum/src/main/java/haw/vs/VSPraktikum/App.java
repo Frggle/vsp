9b52bf95362b806c0a75b808f168d027fd4a07e2
marc <marc.kaepke@gmail.com>
2016-04-12 13:24:35 +0200

    YellowPage Registry -> wird noch nicht im Docker angezeigt

--- VSPraktikum/src/main/java/haw/vs/VSPraktikum/App.java
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/App.java
@@ -1,8 +1,35 @@
 package haw.vs.VSPraktikum;
 import static spark.Spark.*;
+import org.json.JSONObject;
+import com.mashape.unirest.http.Unirest;
+import haw.vs.VSPraktikum.services.Dice;
 
 public class App {
-    public static void main(String[] args) {
+	private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
+	
+	public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+		get("/dice", Dice::roll);
+		
+		try{
+			JSONObject json = new JSONObject();
+			json.put("name", "DiceService");
+			json.put("description", "Gives you a single dice roll");
+			json.put("service", "dice");
+			json.put("uri", "http://apb154_docker_0:4567/dice"); // TODO: URI richtig ???
+			
+			System.err.println(json);
+			
+			String result = Unirest.put(YELLOW_PAGES + "/1")
+					.header("Content-Type", "application/json")
+					.body(json)
+					.asString().getBody();	
+			
+			System.err.println("Result: " + result);
+		} catch (Exception e) {
+			//
+		}
+		
+
     }
 }
--- VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
@@ -5,20 +5,14 @@ import spark.Request;
 import spark.Response;
 
 public class Dice {
-
+	
 	public static Integer roll(Request request, Response response) {
 		
 		response.status(200);
 		response.type("application/json");
 		
 		String player = request.queryParams("player");
-		String game = request.queryParams("game"); 
-		
-		try{
-			
-		} catch(Exception e) {
-			//
-		}
+		String game = request.queryParams("game");
 		
 		return (Integer) ThreadLocalRandom.current().nextInt(1, 7);
 	}
