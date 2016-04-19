e7107d7e74666ee430315a4c1c01d7864378b4fd
Jenny <jenny2209@gmx.de>
2016-04-19 09:25:11 +0200

    Main in Dice gezogen. Ausgabe nun String

--- VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
@@ -1,12 +1,43 @@
 package haw.vs.VSPraktikum.services;
 
+import static spark.Spark.get;
+
 import java.util.concurrent.ThreadLocalRandom;
+
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.Unirest;
+
 import spark.Request;
 import spark.Response;
 
 public class Dice {
 	
-	public static Integer roll(Request request, Response response) {
+	private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
+	
+	public static void main(String[] args) {
+		try{
+			JSONObject json = new JSONObject();
+			json.put("name", "DiceService");
+			json.put("description", "Gives you a single dice roll");
+			json.put("service", "dice");
+			json.put("uri", "http://abp154_docker_0:4567/dice"); // TODO: URI richtig ???
+			
+			System.err.println(json);
+			
+			String result = Unirest.post(YELLOW_PAGES + "/1337")
+					.header("Content-Type", "application/json")
+					.body(json)
+					.asString().getBody();	
+			
+			get("/dice", Dice::roll);
+			System.err.println("Result: " + result);
+		} catch (Exception e) {
+			//
+		}
+    }
+	
+	public static String roll(Request request, Response response) {
 		
 		response.status(200);
 		response.type("application/json");
@@ -14,6 +45,8 @@ public class Dice {
 		String player = request.queryParams("player");
 		String game = request.queryParams("game");
 		
-		return (Integer) ThreadLocalRandom.current().nextInt(1, 7);
+		Integer randomNumber = (Integer) ThreadLocalRandom.current().nextInt(1, 7);
+		String output = "{\"number\": " + randomNumber + "}";
+		return output;
 	}
 }
