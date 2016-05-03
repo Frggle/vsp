ed64c6cea9f4b4b67a84bf7f8ce7fbee0ef07ec5
Marc Kaepke <marc.kaepke@gmail.com>
2016-05-03 13:31:47 +0200

    Praktikum 1 abgeschlossen

--- VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
+++ VSPraktikum/src/main/java/haw/vs/VSPraktikum/services/Dice.java
@@ -1,31 +1,83 @@
 package haw.vs.VSPraktikum.services;
 
 import static spark.Spark.get;
-import static haw.vs.VSPraktikum.util.YellowService.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ThreadLocalRandom;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-
+import org.json.JSONObject;
+import haw.vs.VSPraktikum.util.EventServiceProvider;
+import haw.vs.VSPraktikum.util.YellowpagesService;
 
 public class Dice {
 	
 	public static void main(String[] args) {
-		registerService("DiceService", "Gives you a single dice roll", "dice", "/1337");
-		
 		get("/dice", (request, response) -> {
 			response.status(200);
 			response.type("application/json");
 			
-			Gson gson = new Gson();
+			String player = request.queryParams("player");
+			String game = request.queryParams("game");
 			
-			String player = request.params("player");
-			String game = request.params("game");
+			createEvent(player, game);
 			
 			Integer randomNumber = (Integer)ThreadLocalRandom.current().nextInt(1, 7);
 			
-			String output = "{\"number\": " + randomNumber + "}";
-			JsonElement jsonElem = gson.fromJson(output, JsonElement.class);
-			return jsonElem.getAsJsonObject();
+			JSONObject json = new JSONObject();
+			json.put("number", randomNumber);
+			json.put("player", player);
+			json.put("game", game);
+			return json.toString();
 		});
+	}
+	
+	private static String createEvent(String player, String game) {
+		HttpURLConnection connection = null;
+		
+		try {
+			YellowpagesService eventService = EventServiceProvider.getService();
+			URL url = new URL(eventService.getUri() + "events");
+			
+			String requestBody = "{ " + "\"game\":\"" + game + "\", " + "\"player\":\"" + player + "\"" + "}";
+			
+			connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Content-Length", "" + requestBody.getBytes().length);
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			
+			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+			wr.writeBytes(requestBody);
+			wr.flush();
+			wr.close();
+			
+			int responseCode = connection.getResponseCode();
+			
+			InputStream is = connection.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			StringBuilder response = new StringBuilder();
+			String line;
+			while((line = reader.readLine()) != null) {
+				response.append(line);
+				response.append('\r');
+			}
+			
+			reader.close();
+			return response.toString();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(connection != null) {
+				connection.disconnect();
+			}
+		}
+		
+		return "";
 	};
 }
